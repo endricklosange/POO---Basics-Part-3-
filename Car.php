7f5b0ec87581a138e6b2482d97c8c97df2325421
endricklosange <69051334+endricklosange@users.noreply.github.com>
2021-04-05 14:23:09 +0200

    Add files via upload

---
+++ Car.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'Vehicle.php';
+class Car extends Vehicle
+{
+  
+  private string$typeEnergy;
+  public const ALLOWED_ENERGIES =['fuel',
+  'electric'];
+  
+  private int $fuelLevel;
+
+  public function __construct(string $color, int $numberseats, string $typeEnergy)
+  {
+    $this->$color = $color;
+    $this->$numberseats = $numberseats;
+    $this->setEnergy($typeEnergy);
+  }
+  public function setEnergy(string $energy): Car
+{
+    if (in_array($energy, self::ALLOWED_ENERGIES)) {
+        $this->energy = $energy;
+    }
+    return $this;
+}
+}   
+
+
+  
+
